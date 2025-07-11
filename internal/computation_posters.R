64738afe77efed5373254ddb373b3da87d5e476e
DamienD <damien.dupre.ac@gmail.com>
2025-07-11 10:58:18 +0100

    poster group

--- internal/computation_posters.R
+++ internal/computation_posters.R
@@ -24,16 +24,23 @@ posters <-
     authors = x5
   ) |> 
   mutate(
+    nb = case_match(
+      nb, 
+      "618447" ~ "Emotions in Daily Life: Interpersonal Perspectives Across Contexts and Cultures - Chair: Davide Pirrone",
+      "618540" ~ "In sync or not: What are the correlates of physiological synchronicity? - Chair: Hedwig Eisenbarth",
+      .default = nb
+    ),
     temp = case_when(
       str_starts(nb, "Poster groups") ~ "poster",
       str_starts(nb, "Thursday") | str_starts(nb, "Friday") ~ "talk",
-      .default = NA_character_),
+      .default = NA_character_
+    ),
     day = paste("Poster Session", cumsum(case_when(temp == "poster" ~ 1, .default = 0))),
     group = if_else(str_starts(nb, "6"), NA_character_, nb)
   ) |> 
   fill(temp, .direction = "down") |> 
   fill(group, .direction = "down") |> 
   filter(temp == "poster") |> 
-  drop_na(title) |> 
+  drop_na(authors) |> 
   remove_empty("cols") |> 
   left_join(submissions, by = join_by(nb))
